ac0abd5a6235d110880bbc8a9375146bd7fb70c8
Etherdrake <67021215+Etherdrake@users.noreply.github.com>
2025-01-06 14:32:28 -0500

    Add PancakeSwapV2 / PancakeSwapV3 custom functionalities

---
+++ eth_defi/pancakeswap_v2/constants.py
@@ -0,0 +1,34 @@
+PANCAKESWAP_V2_DEPLOYMENTS = {
+    "bsc": {
+        "factory": "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+        "router": "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+    },
+    "eth": {
+        "factory": "0x1097053Fd2ea711dad45caCcc45EfF7548fCB362",
+        "router": "0xEfF92A263d31888d860bD50809A8D171709b7b1c",
+    },
+    "zkEVM": {
+        "factory": "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",
+        "router": "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb",
+    },
+    "zkSync": {
+        "factory": "0xd03D8D566183F0086d8D09A84E1e30b58Dd5619d",
+        "router": "0x5aEaF2883FBf30f3D62471154eDa3C0c1b05942d",
+    },
+    "arbitrum": {
+        "factory": "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",
+        "router": "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb",
+    },
+    "linea": {
+        "factory": "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",
+        "router": "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb",
+    },
+    "base_network": {
+        "factory": "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",
+        "router": "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb",
+    },
+    "opBNB": {
+        "factory": "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",
+        "router": "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb",
+    },
+}
